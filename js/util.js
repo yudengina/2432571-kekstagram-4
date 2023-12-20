fe8bd42187feb635eb476e9f950b7854bb8e4804
Юлия Деньгина <julia.dengina77@gmail.com>
2023-12-20 21:26:31 +0500

    5.14. Модуляция

---
+++ js/util.js
@@ -0,0 +1,11 @@
+import './data.js';
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export {getRandomInteger};
+
